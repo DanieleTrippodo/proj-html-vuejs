0a4f64bee1873ffe011f3616bc0f84b664faaea7
DanieleTrippodo <trippododaniele@gmail.com>
2024-06-19 13:44:21 +0200

    Added Jumbotron Component

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,120 @@
+<!-- Qui va la logica del componente (Vue3 + Javascript) -->
+<script>
+import ReadMore from './ReadMore.vue'
+
+export default {
+  name: 'Jumbotron',
+  data() {
+    return {
+      currentSlide: 0,
+      slides: [
+        {
+          image: './src/assets/img/sfondo1.png',
+          title: 'Instrumental Rock',
+          description: 'Music of the Spirit',
+        },
+        {
+          image: './src/assets/img/sfondo2.png',
+          title: 'Instrumental Rock',
+          description: 'Music in this Video',
+        },
+      ]
+    };
+  },
+  methods: {
+    nextSlide() {
+      this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+    },
+    prevSlide() {
+      this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+    }
+  }
+};
+</script>
+
+
+
+<!-- Qui va il contenuto di questo elemento (HTML) -->
+<template>
+  <div :style="{ backgroundImage: `url(${slides[currentSlide].image})` }" class="jumbotron">
+    <div class="content">
+      <h1>{{ slides[currentSlide].title }}</h1>
+      <p>{{ slides[currentSlide].description }}</p>
+
+      <ReadMore> Read More </ReadMore>
+
+    </div>
+
+    <div class="arrows">
+      <span class="arrow left" @click="prevSlide">&#9664;</span>
+      <span class="arrow right" @click="nextSlide">&#9654;</span>
+    </div>
+  </div>
+</template>
+
+
+
+<!-- Qui va lo stile CSS di questo elemento (CSS) -->
+<style scoped>
+.jumbotron {
+  position: relative;
+  width: 100%;
+  height: 47rem;
+  background-size: cover;
+  background-position: center;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  transition: background-image 0.5s ease-in-out;
+}
+.content {
+  z-index: 2;
+}
+h1 {
+  color: orange;
+  margin-bottom: 2rem;
+  font-size: 2.2em;
+}
+p {
+  color: white;
+  font-size: 5em;
+  margin-bottom: 3rem;
+}
+ReadMore {
+  padding: 1rem 3rem;
+  background-color: none;
+  border: 1px solid orange;
+  color: white;
+  font-size: 1.3em;
+  cursor: pointer;
+}
+
+.arrows {
+  position: absolute;
+  top: 50%;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  transform: translateY(-50%);
+}
+.arrow {
+  font-size: 1rem;
+  color: white;
+  cursor: pointer;
+  user-select: none;
+
+  padding-top: .8rem;
+  padding-bottom: .8rem;
+  padding-right: 1rem;
+  padding-left: 1rem;
+  background-color: black;
+}
+.arrow.left {
+  margin-left: 10px;
+}
+.arrow.right {
+  margin-right: 10px;
+}
+</style>
--- src/components/ReadMore.vue
+++ src/components/ReadMore.vue
@@ -9,7 +9,10 @@
 <style scoped>
 button{
   background-color: none;
+  width: 100px;
+  height: 100px;
   border: 2px solid orange;
   padding: 1rem;
+  z-index: 3;
 }
 </style>
