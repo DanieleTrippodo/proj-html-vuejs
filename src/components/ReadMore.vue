b854ced8a62157ee4da8e9f74bedd1706b7211b4
DanieleTrippodo <trippododaniele@gmail.com>
2024-06-19 12:32:09 +0200

    general button ''ReadMore''

---
+++ src/components/ReadMore.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <button> READ MORE </button>
+</template>
+
+<style scoped>
+button{
+  background-color: none;
+  border: 2px solid orange;
+  padding: 1rem;
+}
+</style>
