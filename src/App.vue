b31a3900f83b2233e6536ab7bf8fbddacab61656
Christianmagni97 <christianmagni28@gmail.com>
2024-06-19 19:56:47 +0200

    Aggiunto sense the jazz

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 import Jumbotron from './components/Jumbotron.vue' /* Ricordatevi di importare i vostri componenti, una volta finiti */
 import MainFooter from './components/MainFooter.vue'
 import AlbumOutNow from './components/AlbumOutNow.vue'
+import SenseTheJazz from './components/SenseTheJazz.vue';
+
 </script>
 
 <template>
@@ -13,6 +15,9 @@ import AlbumOutNow from './components/AlbumOutNow.vue'
 
 
   <Jumbotron/>
+
+  <SenseTheJazz/>
+
 
   <AlbumOutNow/>
 
