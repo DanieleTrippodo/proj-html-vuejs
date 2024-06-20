3630454db98069253cb714f8b88c426241e1d30c
SimoneColombo1 <simcolombo2@gmail.com>
2024-06-20 15:51:27 +0200

    update cards

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,63 +1,69 @@
 <script>
+
+
+export default{
+data() {
+    return {
+        CardImage: [
+              {
+              image:"gallery1.jpg",
+              title:'man singing',
+              description:'A ROLL AND BANG THE FLOOR'
+              },
+              {
+               image:'gallery2.jpg',
+               title:'microphone',
+              description:'YOUR MELODY TO OUR MUSIC'
+
+              },
+              {
+                image:'gallery3.jpg',
+                title:'drums',
+              description:'TOUCHING HEAVEN FREQUENCY'
+              },
+              {
+                image:'gallery4.jpg',
+                title:'man playng guitar back',
+             description:'MUSIC MAKES STRONGE BEATS'
+              },
+              {
+                image:'gallery5.jpg',
+                title:'man playng guitar front',
+                description:'SOUND IS THE SPARK OF TIME'
+              },
+              {
+                image:'gallery6.jpg',
+                title:'guitar image',
+                description:'MAKE YOUR NIGHT GROOVY'
+              }
+
+
+        ]
+    }
+},
+
+}
+
 </script>
 
+
+
 <template>
- 
-                <div class="card">
-                    <div class="image">
 
-                    </div>
-                   <span><h3>Lorem ipsum dolor sit amet </h3>
-                        <p> Lorem ipsum dolor: O O O O </p>
-                </span>
-
-                </div>
-
-                   <div class="card">
-                               
-                    <div class="image">
-
-</div>
-<span><h3>Lorem ipsum dolor sit amet </h3>
-    <p> Lorem ipsum dolor: O O O O </p>
-</span>
-                    
-                   </div>
-                      <div class="card">
-                        <div class="image">
-
-                              </div>
-                              <span><h3>Lorem ipsum dolor sit amet </h3>
-                              <p> Lorem ipsum dolor: O O O O </p>
-                              </span>
-                      </div>
-                          <div class="card">
-                            <div class="image">
-
-                              </div>
-                              <span><h3>Lorem ipsum dolor sit amet </h3>
-                                  <p> Lorem ipsum dolor: O O O O </p>
-                              </span>
-                          </div>
-                               <div class="card">
-                                <div class="image">
-
-                                 </div>
-                                   <span><h3>Lorem ipsum dolor sit amet </h3>
-                                          <p> Lorem ipsum dolor: O O O O </p>
-                                                        </span>
-                                        </div>
-                                        <div class="card">
-                                            <div class="image">
-
-                                     </div>
-                                     <span><h3>Lorem ipsum dolor sit amet </h3>
-                                           <p> Lorem ipsum dolor: O O O O </p>
+                                        <div class="card" v-for="(card, index) in CardImage" :key="index">
+                                            
+                                                       <img :src=" '../src/assets/img/' + card.image"  :alt="card.title"> 
+                                    
+                                     <span><h3> {{ card.description }}</h3>
+                                           <p> Stream On: <i class="fa-brands fa-spotify  icon"></i> <i class="fa-brands fa-youtube  icon"></i> <i class="fa-solid fa-music icon"></i> <i class="fa-brands fa-soundcloud icon"></i>   <i class="fa-brands fa-safari  icon"></i> </p>
                                         </span> 
+
+                                       
                                         </div>
+                                  
                                         
 </template>
-<style lang="scss" scoped>
+<style scoped>
 .card{
    
     width: 25rem ;
@@ -67,16 +73,29 @@ display: flex;
     display: flex;
     flex-direction: column;
    
-}
-.card span{
+
+ span{
 
     margin: 1rem 0 0 5rem;
 }
+  
 
-.image{
+
+img{
     width: 25rem;
     height: 25rem;
-    background-color: gray;
+    
+    
+   
+}
 }
 
+.icon{
+  margin-left:0.5rem;
+   
+  &:hover{
+  color:orange;
+  
+}
+}
 </style>
