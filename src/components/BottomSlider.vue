a5acf862903899d029fc99f48ee9a37a924f1881
MirkoValle <mirko.valle20@gmail.com>
2024-06-21 19:16:11 +0200

    Edited layout

--- src/components/BottomSlider.vue
+++ src/components/BottomSlider.vue
@@ -8,47 +8,48 @@ export default{
                     img: "hip_hop_wired_to_dance_make.jpg",
                     title: "Hip Hop Wired to Dance Make",
                     date: "06 November  2021",
-                    text: "Driving short distances music is a music genre that includes traditional folk music and the contemporary genre that evolved from the former",
+                    text: "Driving Short Distances Music is a Music Genre That Includes Traditional Folk Music and The Contemporary Genre That Evolved From The Former",
                 },
                 {
                     img: "music_business-worldwide.jpg",
                     title: "Music Business Worldwide",
                     date: "27 May 2022",
-                    text: "Accelerate work and drive productivity The Beatles' experimentation and creative freedom led other bands to experiment in various ways as well.",
+                    text: "Accelerate Work and Drive Productivity The Beatles' Experimentation and Creative Freedom Led Other Bands to Experiment in Various Ways as Well.",
                 },
                 {
                     img: "giveaways_rock.jpg",
                     title: "Giveaways Rock to All",
                     date: "07 May 2022",
-                    text: "Signs Your Car Battery Has To Be Replaced When the dashboard lights start flashing, this is a sign the battery is dying. Several issues arise and",
+                    text: "Signs Your Car Battery Has To Be Replaced When The Dashboard Lights Start Flashing, This is a Sign the Battery is Dying. Several Issues Arise and",
                 },
                 {
                     img: "drowned_in_sound_feel_joy.jpg",
                     title: "Drowned in Sound Fell Joy",
                     date: "10 April 2022",
-                    text: "Different Types of Music Content For most music creators, they take their work very seriously because of the emotional attachment they may harbor",
+                    text: "Different Types of Music Content For Most Music Creators, They Take Their Work Very Seriously Because of The Emotional Attachment They May Harbor",
                 },
                 {
                     img: "dancing_astronaut_bounce_ipsum.jpg",
                     title: "Dancing Astrounat Bounce Ipsum",
                     date: "01 March 2022",
-                    text: "Music Promotion Channels There are really a lot of music promotion channels out there. Being a music promotion channel involves a lot of work and",
+                    text: "Music Promotion Channels There are Really a Lot of Music Promotion Channels Out There. Being a Music Promotion Channel Involves a Lot of Work and",
                 },
                 {
                     img: "consequence_of_sound_make_us.jpg",
                     title: "Consequence of Sound Make Us",
                     date: "14 February 2022",
-                    text: "Electronic Dance Music Rhythm and Blues Before this, R & B was known as 'race music,' a term that originated in the African American",
+                    text: "Electronic Dance Music Rhythm and Blues Before This, R & B Was Known as 'Race Music,' a Term That Originated in The African American",
                 },
                 {
                     img: "artist_development_and_production.jpg",
                     title: "Artist Development and Production",
                     date: "07 December 2021",
-                    text: "Characterised by its own beat patterns, the lyrics feature what is often extreme violence snd talk of criminal acts. As a result, there's been",
+                    text: "Characterised by Its Own Beat Patterns, the Lyrics Feature What is Often Extreme Violence and Talk of Criminal Acts. As a Result, There's Been",
                 },
             ]
         }
-    }
+    },
+    
 }
 </script>
 
@@ -58,16 +59,16 @@ export default{
         <p>music blog</p>
         <h2>best music blog</h2>
     </div>
-    <button class="left"><i class="fa-solid fa-chevron-left "></i></button>
-    <button class="right"><i class="fa-solid fa-chevron-right "></i></button>
+    <button class="left"><img src="../assets/img/left-arrow.svg" alt=""></button>
+    <button class="right"><img src="../assets/img/right-arrow.svg" alt=""></button>
     <div class="slider">
         <article v-for="(element, index) in sliderElements">
-            <div>
+            <div class="cover">
                 <img :src="'../src/assets/img/' + element.img" alt="">
             </div>
             <div class="info">
                 <h3>{{ element.title }}</h3>
-                <h4>{{ element.date }}</h4>
+                <h4><img src="../assets/img/date.svg" alt="">{{ element.date }}</h4>
                 <p>{{ element.text }}</p>
             </div>
         </article>
@@ -82,17 +83,33 @@ section{
     flex-direction: column;
     max-width: 1400px;
     margin: auto;
-    padding: 5rem 0;
+    padding: 7rem 0;
     position: relative;
+
+    .cover {
+
+        overflow: hidden;
+        img{
+            transition: transform .5s;
+            cursor: pointer;
+            &:hover{
+                transform: scale(1.1);
+            }
+        }
+    }
 
     button{
         position: absolute;
         top: 51%;
         padding: 1rem;
-        font-size: 2rem;
         background-color: transparent;
         border: none;
         color: white;
+
+        img{
+            width: 1rem;
+            cursor: pointer;
+        }
     }
 
     button.left{
@@ -109,24 +126,25 @@ section{
             color: #e37f0a;
             text-transform: uppercase;
             text-align: center;
-            font-size: 1.2rem;
+            font-size: 1rem;
+            font-family: "Open-sans", sans-serif;
+            font-weight: 600;
         }
 
         h2{
             color: white;
             text-transform: uppercase;
             text-align: center;
-            font-size: 5rem;
+            font-size: 3.9rem;
         }
     }
 
     div.slider{
         display: flex;
         overflow: scroll;
-        max-width: 1400px;
         scrollbar-width: none;
         article{
-            min-width: calc(100% / 3);
+            min-width: calc((100% / 3) - 1.7rem);
             margin: 1rem;
 
             img{
@@ -137,17 +155,33 @@ section{
             
                 h3{
                     color: white;
-                    font-size: 2rem;
+                    font-size: 1.5rem;
+                    text-transform: uppercase;
+                    margin: 1rem 0;
+                    cursor: pointer;
+                    &:hover{
+                        color: #e37f0a;
+                    }
                 }
 
                 h4{
                     color: #e37f0a;
-                    font-size: 1.2rem;
+                    font-size: 1rem;
+                    margin-bottom: 1rem;
+                    font-family: "Open-sans", sans-serif;
+                    font-weight: 100;
+                    img{
+                        width: .9rem;
+                        margin-right: .5rem;
+                    }
                 }
 
                 p{
                     color: #b6b6b6;
-                    font-size: 1.2rem
+                    font-size: 1rem;
+                    font-family: "Open-sans", sans-serif;
+                    font-weight: 100;
+                    line-height: 1.8rem;
                 }
             }
         };
