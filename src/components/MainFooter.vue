de2f1fe0d3d507bb1e497ad934cb939dedc35584
MirkoValle <mirko.valle20@gmail.com>
2024-06-24 13:37:44 +0200

    Fixed footer

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -102,7 +102,7 @@ export default {
   font-size: 1.4rem;
 }
 .container {
-  max-width: 1200px;
+  max-width: 1400px;
   margin: 0 auto;
   padding: 0 20px;
   margin-bottom: 10rem;
@@ -130,16 +130,21 @@ export default {
 }
 .footer-column address {
   line-height: 1.6;
+  font-family: "open-sans", sans-serif;
+  color: #c5c5c5;
+  font-size: 1rem;
 }
 .footer-column ul li {
-  margin-bottom: 10px;
+  margin-bottom: 5px;
 }
 .footer-column ul li a {
-  color: #fff;
+  color: #c5c5c5;
   text-decoration: none;
+  font-family: "open-sans", sans-serif;
+  font-size: 1rem;
 }
 .footer-column ul li a:hover {
-  text-decoration: underline;
+  color: #ff8800;
 }
 .footer-column .social-icons a {
   display: inline-block;
@@ -148,23 +153,30 @@ export default {
   text-decoration: none;
 }
 .footer-column .social-icons a i {
-  font-size: 1.5em;
+  font-size: .7em;
+  margin-right: 1rem;
 }
 .footer-column form input,
 .footer-column form textarea {
   width: 100%;
-  padding: 10px;
+  padding: 15px;
   margin-bottom: 10px;
-  border: none;
-  background-color: #333;
-  color: #fff;
+  border: 1px solid #3a3a3a;
+  background-color: transparent;
+  color: #a1a1a1;
+  font-size: 1rem;
+}
+.footer-column form textarea{
+  height: 6rem;
 }
 .footer-column form button {
-  padding: 10px 20px;
-  background-color: orange;
-  border: none;
+  padding: 10px 40px;
+  background-color: transparent;
+  border: 1px solid  #ff8800;
   color: #fff;
   cursor: pointer;
+  font-size: .9rem;
+  font-weight: 600;
 }
 .footer-column form button:hover {
   background-color: #ff8800;
@@ -172,6 +184,9 @@ export default {
 .footer-bottom {
   text-align: center;
   margin-top: 20px;
+  font-family: "open-sans", sans-serif;
+  color: #c5c5c5;
+  font-size: 1rem;
 }
 
 hr{
